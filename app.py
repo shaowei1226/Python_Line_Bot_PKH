148d718c181a024235c4d5489b570a92986ad8aa
SHW <114381672+shaowei1226@users.noreply.github.com>
2024-09-02 11:12:46 +0800

    three

--- app.py
+++ app.py
@@ -24,35 +24,22 @@ handler = WebhookHandler(os.getenv('handler'))
 
 # 監聽所有來自 /callback 的 Post Request
 @app.route("/callback", methods=['POST'])
-def callback(event):
-    user_message = event.message.text
-    if user_message == "手牌紀錄":
-        # 格式化的手牌記錄模板
-        reply_message = (
-            "Level: \n"
-            "玩家人數: \n"
-            "Hero 位置: \n"
-            "Hero 後手: \n"
-            "其他玩家後手: \n\n"
-            "Hero 手牌: \n"
-            "翻前Action: \n"
-            "Flop 開的牌: \n"
-            "Flop Action: \n"
-            "Turn 開的牌: \n"
-            "Turn Action: \n"
-            "River 開的牌: \n"
-            "River Action: \n"
-        )
-        line_bot_api.reply_message(
-            event.reply_token,
-            TextSendMessage(text=reply_message)
-        )
+def callback():
+    signature = request.headers['X-Line-Signature']
+    body = request.get_data(as_text=True)
 
-@handler.add(MessageEvent, message=TextMessage)
-def handle_message(event):
-    user_message = event.message.text
-    if user_message == "手牌紀錄":
-        # 格式化的手牌記錄模板
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+
+    return 'OK'
+
+@handler.add(PostbackEvent)
+def handle_postback(event):
+    data = event.postback.data
+    if 'action=record_hand' in data:
+        
         reply_message = (
             "Level: \n"
             "玩家人數: \n"
@@ -67,11 +54,8 @@ def handle_message(event):
             "Turn Action: \n"
             "River 開的牌: \n"
             "River Action: \n"
-        )
-        line_bot_api.reply_message(
-            event.reply_token,
-            TextSendMessage(text=reply_message)
-        )
+            )     
+        
     
     
 import os
